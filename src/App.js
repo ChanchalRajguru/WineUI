b26f3764f00780898638f2be03ff796b3fd24725
ChanchalRajguru <rajguruchanchal@gmail.com>
2020-10-07 17:44:08 +0100

    created wineSummaryItem component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import WineSummaryItem from "./WineSummaryItem/WineSummaryItem";
 
 class App extends Component {
   render() {
-    const someMessage = 'A new react app message';
+    const someMessage = "A new react app message";
     return (
       <div>
-        <h1>{someMessage + 2}</h1>
+        <WineSummaryItem />
+        <WineSummaryItem />
+        <WineSummaryItem />
+        <WineSummaryItem />
       </div>
     );
     // return React.createElement('h1', null, "Hello from my very first react app updated.");
---
+++ src/WineSummaryItem/WineSummaryItem.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const wineSummaryItem = () =>{
+    return <p>2007 Rioja </p>
+};
+
+
+export default wineSummaryItem;
