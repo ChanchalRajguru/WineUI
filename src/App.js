ece855d756b0d03a4ba1b50f13d8e6e5d893e49e
ChanchalRajguru <rajguruchanchal@gmail.com>
2020-10-08 20:26:03 +0100

    Added Catch Statement
    If there is no response from the server the catch function will handle it and return an appropriate response.

--- src/App.js
+++ src/App.js
@@ -7,17 +7,25 @@ import WineSummaryItem from "./WineSummaryItem/WineSummaryItem";
 class App extends Component {
   state = {
     winesummaries: [],
+    fetchError: false,
   };
 
   //Axios is used for REST endpoints
   fetchWineSummaries = () => {
-    axios.get("http://localhost:8080/wines/summary/").then((response) => {
-      this.setState({
-        winesummaries: response.data,
-      });
-      console.log(response.data);
-    });
-  };
+    axios
+      .get("http://localhost:8080/wines/summary/")
+      .then((response) => {
+        this.setState({
+          winesummaries: response.data,
+        });
+      })
+      .catch((error) => {
+        console.log(error.response.status);
+        this.setState({
+          fetchError: true
+        })
+      })
+  }
 
   //componentDidMount is react lifecycle method.
   componentDidMount() {
@@ -25,18 +33,15 @@ class App extends Component {
   }
 
   render() {
+    let wineSummariesList = <p>Unable to fetch wine summaries</p> 
+    if(!this.state.fetchError){
+       wineSummariesList = this.state.winesummaries.map(wineSummary => {
+         return <WineSummaryItem key={wineSummary.id} year={wineSummary.year} name={wineSummary.name}/>
+       }) 
+    }
     return (
       <div>
-        {this.state.winesummaries.map((wineSummary) => {
-          //key is an in-built property.
-          return (
-            <WineSummaryItem
-              key={wineSummary.id}
-              year={wineSummary.year}
-              name={wineSummary.name}
-            />
-          );
-        })}
+          {wineSummariesList}
       </div>
     );
     // return React.createElement('h1', null, "Hello from my very first react app updated.");
